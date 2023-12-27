8d5b5f115d359bb1d192145db550e5c478632404
unknown <alvesmlktop@gmail.com>
2023-12-27 18:46:29 -0300

    controller do funcionario implementado e listar e criar do crud, funcionario service com funcoes para listar e criar do crud

--- src/main/java/com/BarApi/Dev/controllers/FuncionarioController.java
+++ src/main/java/com/BarApi/Dev/controllers/FuncionarioController.java
@@ -1,2 +1,39 @@
-package com.BarApi.Dev.controllers;public class FuncionarioController {
+package com.BarApi.Dev.controllers;
+
+import com.BarApi.Dev.domain.Funcionario;
+import com.BarApi.Dev.domain.dto.FuncionarioCadastrarDto;
+import com.BarApi.Dev.domain.dto.FuncionarioListarDto;
+import com.BarApi.Dev.services.FuncionarioServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value ="/funcionarios")
+public class FuncionarioController {
+@Autowired
+    FuncionarioServices service;
+
+    @PostMapping
+    @Transactional
+
+    public ResponseEntity cadastrarFuncionario (@RequestBody FuncionarioCadastrarDto dados, UriComponentsBuilder uriBuilder){
+       service.cadastrarFuncionario(dados);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FuncionarioListarDto>> findAll(){
+        List<Funcionario> list = service.findAll();
+        List<FuncionarioListarDto> listDTO = list.stream().map(obj -> new FuncionarioListarDto(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+
+    }
 }
--- src/main/java/com/BarApi/Dev/services/FuncionarioServices.java
+++ src/main/java/com/BarApi/Dev/services/FuncionarioServices.java
@@ -1,6 +1,52 @@
-package com.BarApi.Dev.domain.services;
+package com.BarApi.Dev.services;
 
+import com.BarApi.Dev.domain.Funcionario;
+import com.BarApi.Dev.domain.dto.FuncionarioCadastrarDto;
+import com.BarApi.Dev.domain.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class FuncionarioServices {
+    @Autowired
+    FuncionarioRepository repository;
 
+    public List<Funcionario> findAll() {
+        return repository.findAll();
+    }
+
+    public void  cadastrarFuncionario(FuncionarioCadastrarDto dados) {
+        Funcionario funcionarioCadastro = converterCadastroEntidade(dados);
+       repository.save(funcionarioCadastro);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+        private Funcionario converterCadastroEntidade(FuncionarioCadastrarDto cadastro){
+
+        Funcionario funcionario = new Funcionario();
+
+            funcionario.setNome(cadastro.nome());
+            funcionario.setSobrenome(cadastro.sobrenome());
+            funcionario.setCpf(cadastro.cpf());
+            funcionario.setTelefone(cadastro.telefone());
+            funcionario.setEmail(cadastro.email());
+            funcionario.setSenha(cadastro.senha());
+            funcionario.setFuncao(cadastro.funcao());
+
+
+            return funcionario;
+        }
 }
+
