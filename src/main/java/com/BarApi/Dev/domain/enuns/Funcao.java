c574316e5c4d972eca664d0a038229365571bf4e
unknown <alvesmlktop@gmail.com>
2023-12-27 18:45:09 -0300

    enum para função e repository para funcionario implementados

--- src/main/java/com/BarApi/Dev/domain/enuns/Funcao.java
+++ src/main/java/com/BarApi/Dev/domain/enuns/Funcao.java
@@ -1,9 +1,32 @@
-package com.BarApi.Dev.domain.repository;
+package com.BarApi.Dev.domain.enuns;
 
-public enum Funcao {
-    Admin,
-    Gerente,
-    Garcon,
-    Balconista,
-    Portaria
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public enum Funcao  {
+    ADMIN(0, "ROLE_ADMIN"),
+    GERENTE(1, "ROLE_GERENTE"),
+    GRACON(2, "ROLE_GARCON"),
+    BRALCONISTA(3, "ROLE_BALCONISTA"),
+    PORTARIA(4, "ROLE_PORTARIA");
+    private Integer codigo;
+    private String descricao;
+
+    Funcao(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+    public static Funcao toEnum(Integer cod){
+        if (cod == null){
+            return null;
+        }
+        for (Funcao x :Funcao.values()){
+            if(cod.equals(x.getCodigo())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Funcao Invalido");
+    }
 }
--- src/main/java/com/BarApi/Dev/domain/repository/FuncionarioRepository.java
+++ src/main/java/com/BarApi/Dev/domain/repository/FuncionarioRepository.java
@@ -1,2 +1,9 @@
-package com.BarApi.Dev.domain.repository;public interface FuncionarioRepository {
+package com.BarApi.Dev.domain.repository;
+
+import com.BarApi.Dev.domain.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 }
