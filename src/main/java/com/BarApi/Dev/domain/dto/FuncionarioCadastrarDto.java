540fd3ca80192f6f5e3090e35f557229fe79072e
unknown <alvesmlktop@gmail.com>
2023-12-27 18:40:42 -0300

    dto funcionario-cadastro e listar criado

--- src/main/java/com/BarApi/Dev/domain/dto/FuncionarioCadastrarDto.java
+++ src/main/java/com/BarApi/Dev/domain/dto/FuncionarioCadastrarDto.java
@@ -1,2 +1,17 @@
-package com.BarApi.Dev.domain.dto;public record FuncionarioCadastrarDto() {
+package com.BarApi.Dev.domain.dto;
+
+import com.BarApi.Dev.domain.enuns.Funcao;
+
+import java.util.Set;
+
+public record FuncionarioCadastrarDto(
+
+        String nome,
+        String sobrenome,
+        String cpf,
+        String telefone,
+        String email,
+        String senha,
+        Funcao funcao
+        ){
 }
--- src/main/java/com/BarApi/Dev/domain/dto/FuncionarioListarDto.java
+++ src/main/java/com/BarApi/Dev/domain/dto/FuncionarioListarDto.java
@@ -1,2 +1,38 @@
-package com.BarApi.Dev.domain.Dto;public record FuncionarioListarDto() {
+package com.BarApi.Dev.domain.dto;
+
+import com.BarApi.Dev.domain.Funcionario;
+import com.BarApi.Dev.domain.enuns.Funcao;
+
+import java.time.LocalDate;
+
+public record FuncionarioListarDto(Long id,
+                                   String nome,
+                                   String sobrenome,
+                                   String cpf,
+                                   String telefone,
+                                   String email,
+                                   String senha,
+                                   Funcao funcao,
+                                   LocalDate dataCriacao)
+
+{
+
+
+
+        public FuncionarioListarDto(Funcionario funcionario) {
+
+
+            this(
+                    funcionario.getId(),
+                    funcionario.getNome(),
+                    funcionario.getSobrenome(),
+                    funcionario.getCpf(),
+                    funcionario.getTelefone(),
+                    funcionario.getEmail(),
+                    funcionario.getSenha(),
+                    funcionario.getFuncao(),
+                    funcionario.getDataCriacao()
+            );
+    }
 }
+
